a3669ff8ec80d4273c8f22d488e6adf28fdce050
Beyshin <thedemexus@gmail.com>
2026-04-20 20:34:53 +0200

    added primary tables to main database

--- backend/services/primaryDb.js
+++ backend/services/primaryDb.js
@@ -9,15 +9,46 @@ class PrimaryDatabase {
         this.init();
     }
 
-    init(){
+    init() {
+        // UZYTKOWNICY
         this.connection.prepare(
             `CREATE TABLE IF NOT EXISTS Users(
-            user_id    INTEGER NOT NULL,
-            mail TEXT NOT NULL UNIQUE,
-            login    TEXT NOT NULL UNIQUE,
-            password    TEXT NOT NULL,
-            PRIMARY KEY(user_id AUTOINCREMENT)
-        );`
+                                                 user_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                                 mail TEXT NOT NULL UNIQUE,
+                                                 login TEXT NOT NULL UNIQUE,
+                                                 password TEXT NOT NULL
+             );`
+        ).run();
+
+        // KANDYDACI
+        this.connection.prepare(
+            `CREATE TABLE IF NOT EXISTS Candidates(
+                                                      candidate_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                                      first_name TEXT NOT NULL,
+                                                      last_name TEXT NOT NULL
+             );`
+        ).run();
+
+        // GLOSOWANIA
+        this.connection.prepare(
+            `CREATE TABLE IF NOT EXISTS Votings(
+                                                   voting_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                                   name TEXT NOT NULL,
+                                                   start_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+             );`
+        ).run();
+
+        // ASOCJACYJNA KANDYDACI-GLOSOWANIE-LICZBA PIERWSZA P(N)
+        this.connection.prepare(
+            `CREATE TABLE IF NOT EXISTS Candidates_Votings(
+                                                              candidate_id INTEGER NOT NULL,
+                                                              voting_id INTEGER NOT NULL,
+                                                              p_value INTEGER NOT NULL,
+                                                              PRIMARY KEY(candidate_id, voting_id),
+                FOREIGN KEY(voting_id) REFERENCES Votings(voting_id),
+                FOREIGN KEY(candidate_id) REFERENCES Candidates(candidate_id),
+                UNIQUE(voting_id, p_value)
+                );`
         ).run();
     }
 
